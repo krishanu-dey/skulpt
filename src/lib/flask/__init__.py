1cb4810e257d44cb38957484a0764e090483d3f3
Krishanu Dey <deyk@tcd.ie>
2022-02-16 23:04:15 +0000

    Small updates

--- src/lib/flask/__init__.py
+++ src/lib/flask/__init__.py
@@ -60,9 +60,9 @@ class Flask():
                         response["html"] = htmlWithParamsMaybe[0]
                         response["template_params"] = htmlWithParamsMaybe[1]
                 except Exception as e:
-                    exc_type, exc_obj, exc_tb = sys.exc_info()
-                    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                    print(exc_type, fname, exc_tb.tb_lineno)
+                    # exc_type, exc_obj, exc_tb = sys.exc_info()
+                    # fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                    # print(exc_type, fname, exc_tb.tb_lineno)
                     
                     print(e)
                     response["html"] = f"<h4> Syntax Error in the '{routeFromTable}' route function of '{requestMethod}' request type.</h4>"\
